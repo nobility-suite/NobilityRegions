72a58df3d5e28be02436d0737ff628894a0947d5
Stephen / Sharp <Sharpcastle33@users.noreply.github.com>
2020-05-17 19:16:59 -0400

    Nodes track workers

--- src/main/java/io/github/kingvictoria/nodes/Node.java
+++ src/main/java/io/github/kingvictoria/nodes/Node.java
@@ -1,24 +1,33 @@
 package io.github.kingvictoria.nodes;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-
-import io.github.kingvictoria.Region;
 
 public class Node {
 	public String name;
 	public int slots;
 	public ArrayList<ItemStack> output;
 	public NodeType type;
+	ArrayList<UUID> workers;
 	
 	public Node(String name, int slots) {
 		this.name = name;
 		this.slots = slots;
 		
 		ArrayList<ItemStack> output = new ArrayList<ItemStack>();
+		ArrayList<UUID> workers = new ArrayList<UUID>();
 		output.add(new ItemStack(Material.STONE));
+	}
+	
+	public boolean addWorker(Player p) {
+		if(workers.size() < slots) {
+			workers.add(p.getUniqueId());
+			return true;
+		}else return false;
 	}
 
 }
